b0dc77a0d09affeccb50d410c551c85dea49367d
GeekerBowl <227068156+guguguTest@users.noreply.github.com>
2025-11-25 03:27:10 +0800

    Add 每日签到页

--- src/router/functions.js
+++ src/router/functions.js
@@ -10,7 +10,6 @@ export default [
             {
                 name: 'Fortune',
                 path: '/fortune',
-                component: () => import('@/views/404.vue'),
                 component: () => import('@/views/Fortune.vue'),
                 meta: {
                     ...PRESET_LOGIN_BS0,
